0b45f137a250eeaefbb505b04f3d860016f208ab
Omer Shem-Tov <omer@docontrol.io>
2022-10-08 00:34:53 +0300

    remove this

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -59,7 +59,7 @@ function Header(props) {
           <img alt="logo" src={logo} />
         </Link>
 
-        {this.props.currentUser ? (
+        {props.currentUser ? (
           <LoggedInView currentUser={props.currentUser} />
         ) : (
           <LoggedOutView currentUser={props.currentUser} />
