bf849fb74c4ee35ae1c7a536c6e6cacaaf7b02ef
Omer Shem-Tov <omer@docontrol.io>
2023-03-11 14:52:50 +0200

    now with the solution

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -1,64 +1,77 @@
-import { create } from "react-test-renderer";
-import { mount } from "enzyme";
-import configureMockStore from "redux-mock-store";
-import CommentInput from "../../components/Item/CommentInput";
+import React, { useState } from "react";
 import agent from "../../agent";
+import { connect } from "react-redux";
 import { ADD_COMMENT } from "../../constants/actionTypes";
+// import { useDispatch } from 'react-redux'
 
-const mockStore = configureMockStore();
-agent.Comments.create = jest.fn();
-
-describe("CommentInput component", () => {
-  let store;
-
-  beforeEach(() => {
-    store = mockStore({});
-  });
-
-  it("Snapshot testing with no user", () => {
-    const component = create(
-      <CommentInput
-        store={store}
-        slug={"slug"}
-        currentUser={{ username: "name", image: "" }}
-      />
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it("Submit text", () => {
-    const user = { username: "name", image: "" };
-    const component = mount(
-      <CommentInput store={store} slug={"slug"} currentUser={user} />
-    );
-    const comment = { id: 1 };
-    agent.Comments.create.mockResolvedValue(comment);
-
-    const event = { target: { value: "sometext" } };
-    component.find("textarea").simulate("change", event);
-    component.find("form").simulate("submit");
-
-    setImmediate(async () => {
-      expect(store.getActions()).toHaveLength(1);
-      expect(store.getActions()[0].type).toEqual(ADD_COMMENT);
-      expect(await store.getActions()[0].payload).toEqual(comment);
-    });
-  });
-
-  it("Clear text after submit", async () => {
-    const user = { username: "name", image: "" };
-
-    const component = mount(
-      <CommentInput store={store} slug={"slug"} currentUser={user} />
-    );
-
-    const comment = { id: 1 };
-    agent.Comments.create.mockResolvedValue(comment);
-
-    const event = { target: { value: "sometext" } };
-    component.find("textarea").simulate("change", event);
-    component.find("form").simulate("submit");
-    expect(component.find("textarea").text()).toHaveLength(0);
-  });
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: (payload) => dispatch({ type: ADD_COMMENT, payload }),
 });
+
+const CommentInput = (props) => {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     body: "",
+  //   };
+
+  //   this.setBody = (ev) => {
+  //     this.setState({ body: ev.target.value });
+  //   };
+
+  //   this.createComment = async (ev) => {
+  //     ev.preventDefault();
+  //     agent.Comments.create(this.props.slug, {
+  //       body: this.state.body,
+  //     }).then((payload) => {
+  //       this.props.onSubmit(payload);
+  //     });
+  //     this.setState({ body: "" });
+  //   };
+  // }
+  // console.log('useDispatch',{useDispatch})
+
+  // const dispatch = useDispatch();
+  const [body, setBody] = useState("");
+
+  const createComment = async (ev) => {
+    ev.preventDefault();
+    console.log("props=", { x: props });
+    agent.Comments.create(props.slug, {
+      body: body,
+    }).then((payload) => {
+      console.log("payload=", payload);
+      // console.log('dispatch',{dispatch})
+      // dispatch({ type: ADD_COMMENT, payload })
+      props.onSubmit(payload);
+    });
+    setBody("");
+  };
+
+  return (
+    <form className="card comment-form m-2" onSubmit={createComment}>
+      <div className="card-block">
+        <textarea
+          className="form-control"
+          placeholder="Write a comment..."
+          value={body}
+          onChange={(ev) => setBody(ev.target.value)}
+          rows="3"
+        ></textarea>
+      </div>
+      <div className="card-footer">
+        <img
+          src={props.currentUser.image}
+          className="user-pic mr-2"
+          alt={props.currentUser.username}
+        />
+        <button className="btn btn-sm btn-primary" type="submit">
+          Post Comment
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default connect(() => ({}), mapDispatchToProps)(CommentInput);
+// export default CommentInput;
