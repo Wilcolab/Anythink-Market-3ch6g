8e562fd3c2e9fdee9f04ff10523b5bb0949610ac
Omer Shem-Tov <omer@docontrol.io>
2023-05-27 16:04:50 +0000

    add global

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import { store, history } from "./store";
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
 import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/GlobalStyle';
 import App from "./components/App";
 
 const theme = {
@@ -16,11 +17,13 @@ const theme = {
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <ThemeProvider theme={theme}>
-        <Switch>
-          <Route path="/" component={App} />
-        </Switch>
-      </ThemeProvider>
+      <GlobalStyle>
+        <ThemeProvider theme={theme}>
+          <Switch>
+            <Route path="/" component={App} />
+          </Switch>
+        </ThemeProvider>
+      </GlobalStyle>
     </ConnectedRouter>
   </Provider>,
 
