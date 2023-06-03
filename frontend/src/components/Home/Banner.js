05e846bd752f06ebaf00d27513b8fe8161192fe9
WilcoApp <hello@wilco.gg>
2023-06-03 18:27:37 +0000

    CircleCI configure

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,22 +1,18 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
-import styled from "styled-components";
-
-const BannerDiv = styled.div`
-background-color: ${props => props.theme.main};
-`;
 
 const Banner = () => {
   return (
-    <BannerDiv>
+    <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
+          <span>A place to </span>
+          <span id="get-part">get</span>
           <span> the cool stuff.</span>
         </div>
       </div>
-    </BannerDiv>
+    </div>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,29 +2,16 @@ import "./custom.scss";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import React from "react";
-import { store, history } from "./store";
+import { store } from "./store";
 
-import { Route, Switch } from "react-router-dom";
-import { ConnectedRouter } from "react-router-redux";
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './styles/GlobalStyle';
 import App from "./components/App";
-
-const theme = {
-  main: "mediumseagreen"
-};
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <GlobalStyle>
-        <ThemeProvider theme={theme}>
-          <Switch>
-            <Route path="/" component={App} />
-          </Switch>
-        </ThemeProvider>
-      </GlobalStyle>
-    </ConnectedRouter>
+    <BrowserRouter basename="/">
+      <App />
+    </BrowserRouter>
   </Provider>,
 
   document.getElementById("root")
